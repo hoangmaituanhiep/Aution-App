04c0ad36cb611a93f98a7b5a30e83d973d874cdf
Heip <hoangmaituanhiep@gmail.com>
2026-03-28 09:07:52 +0700

    Update: gitignore to exclude .class file

---
+++ src/clientHandler.java
@@ -0,0 +1,34 @@
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.file.*;;
+
+public class clientHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        Path indexPath = Paths.get("../webapp/index.jsp");
+
+        String response = "";
+        int statusCode = 200;
+
+        try {
+            String indexContent = Files.readString(indexPath);
+
+            String newDiv = "<div id="testSite">"
+                          + "<h2>Server testing...</h2>";
+            
+            response = indexContent.replace("<div id="testSite">", newDiv);
+        }
+        catch (IOException e){
+            statusCode = 404;
+            response = "<html><body><h1>404 - Cannot find index.jsp file</h1></body></html>";
+            System.out.println("Error reading file: "+e.getMessage());
+        }
+
+        exchange.sendResponseHeaders(statusCode, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
