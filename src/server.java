dfe5b5a21f1d778ef1af0066937c4da9a8200d31
Heip <hoangmaituanhiep@gmail.com>
2026-03-29 14:01:47 +0700

    Update: modify server processor for fxml and update thread limit

--- src/server.java
+++ src/server.java
@@ -2,17 +2,16 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.*;
 
-import com.sun.net.httpserver.*;
-
 public class server {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(5000), 0);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        ServerSocket server = new ServerSocket(5000);
 
-        server.createContext("/", new clientHandler());
+        while (true) {
+            Socket client = server.accept();
+            System.out.println("New client: " + client.getInetAddress());
 
-        server.setExecutor(Executors.newFixedThreadPool(10));
-        server.start();
-
-        System.out.println("Multi-thread server is running at: http://localhost:5000");
+            executor.execute(new clientHandler(client));
+        }
     }
 }
