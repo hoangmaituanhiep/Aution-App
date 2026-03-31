55f88463912eb43e0be24aab75dac2582728075b
hoangmaituanhiep <hoangmaituanhiep@gmail.com>
2026-04-01 06:51:20 +0700

    Update: Move User.java to subfolder for easy management

---
+++ src/main/java/app/functions/User.java
@@ -0,0 +1,20 @@
+package app;
+
+abstract class User extends Entity {
+    protected String userName;
+    protected String password;
+
+    abstract void register();
+
+    abstract void signIn();
+}
+
+class Bidder extends User {
+
+}
+class Admin extends User {
+
+}
+class Seller extends User {
+
+}
