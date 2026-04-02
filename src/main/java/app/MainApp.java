3b07475faf699d0e1de875fc46e1ee14ec7e080f
Heip <hoangmaituanhiep@gmail.com>
2026-04-02 13:21:21 +0700

    Update: add initial logic in MainApp for creating port

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -1,4 +1,23 @@
 package app;
+
+import java.io.IOException;
+
 public class MainApp {
-    
+    public static void main(String[] args) throws IOException {
+        int port = 0;//default
+
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);//Initiate a port on running
+            }
+            catch(NumberFormatException e) {
+                System.err.println("Cannot format argument:" + args[0]);
+                System.exit(1);
+            }
+        }
+        else if (args.length > 1) {
+            System.err.println("Too many arguments given.");//Only need one port
+            System.exit(1);
+        }
+    }
 }
